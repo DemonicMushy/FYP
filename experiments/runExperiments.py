7f1c49857bdf575aae6e20ee29958149801f8f59
damien <47481731+DemonicMushy@users.noreply.github.com>
2022-01-01 12:46:11 +0800

    fix runExperiments.py

--- experiments/runExperiments.py
+++ experiments/runExperiments.py
@@ -1,7 +1,6 @@
 from pickle import load
 import subprocess as sp
-
-from numpy.core.numeric import full
+import os
 
 cmdBase = "python train.py".split()
 cmdNumAdv = "--num-adversaries 3".split()
@@ -14,63 +13,78 @@ cmdExpName = "--exp-name tag_s_base".split()
 cmdRestore = "--restore".split()
 cmdBenchmark = "--benchmark".split()
 
-allCmds = [cmdBase, cmdNumAdv, cmdNumEpisodes, cmdNumUnits, cmdScenario, cmdLoadDir, cmdSaveDir, cmdExpName, cmdRestore, cmdBenchmark]
+allCmds = [
+    cmdBase,
+    cmdNumAdv,
+    cmdNumEpisodes,
+    cmdNumUnits,
+    cmdScenario,
+    cmdLoadDir,
+    cmdSaveDir,
+    cmdExpName,
+    cmdRestore,
+    cmdBenchmark,
+]
+
 
 def generateFullCommand(restore, benchmark):
     fullCmd = []
     for cmd in allCmds:
-        if cmd==cmdLoadDir and not restore:
+        if cmd == cmdLoadDir and not restore:
             continue
-        if cmd==cmdRestore and not restore:
+        if cmd == cmdRestore and not restore:
             continue
-        if cmd==cmdBenchmark and not benchmark:
+        if cmd == cmdBenchmark and not benchmark:
             continue
         fullCmd += cmd
-    return fullCmd
+    return list(map(lambda x: str(x), fullCmd))
+
 
 # train 10,000 then benchmark? till 12,000?
 
 
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
 
     numIterations = 12
 
     numEpisodes = 10000
     numUnits = 64
-    scenario = 'tag_s_base'
+    scenario = "tag_s_base"
 
     initialDir = "./policy-tag_s_base_LONG"
-    initialExpName = 'tag_s_base_LONG'
+    initialExpName = "tag_s_base_LONG"
 
     cmdNumEpisodes[1] = numEpisodes
     cmdNumUnits[1] = numUnits
     cmdScenario[1] = scenario
 
-    with open('log.txt', 'a') as f:
-        loadDir = ''
-        saveDir = ''
+    with open(os.path.join("logs", f"{initialExpName}-log.txt"), "a") as f:
+        loadDir = ""
+        saveDir = ""
         fullCommand = []
         fullCommandBenchmark = []
-        for i in range(1, numIterations+1):
-            expName = initialExpName + f'_{i}0000'
+        for i in range(1, numIterations + 1):
+            expName = initialExpName + f"_{i*numEpisodes}"
             cmdExpName[1] = expName
             if i == 1:
-                saveDir = initialDir + f'_{i}0000/'
+                saveDir = initialDir + f"_{i*numEpisodes}/"
                 cmdSaveDir[1] = saveDir
                 fullCommand = generateFullCommand(restore=False, benchmark=False)
-                fullCommandBenchmark = generateFullCommand(restore=False, benchmark=True)
+                fullCommandBenchmark = generateFullCommand(
+                    restore=False, benchmark=True
+                )
             else:
                 loadDir = saveDir
-                saveDir = initialDir + f'_{i}0000/'
+                saveDir = initialDir + f"_{i*numEpisodes}/"
                 cmdLoadDir[1] = loadDir
                 cmdSaveDir[1] = saveDir
                 fullCommand = generateFullCommand(restore=True, benchmark=False)
-                fullCommandBenchmark = generateFullCommand(restore=False, benchmark=True)
+                fullCommandBenchmark = generateFullCommand(
+                    restore=False, benchmark=True
+                )
 
-            # print(fullCommand)
-            # print(fullCommandBenchmark)
+            print(fullCommand)
+            print(fullCommandBenchmark)
 
-            sp.run(fullCommand.split(), stdout=f, text=True)
-            sp.run(fullCommandBenchmark.split(), stdout=f, text=True)
+            sp.run(fullCommand, stdout=f, text=True)
+            sp.run(fullCommandBenchmark, stdout=f, text=True)
